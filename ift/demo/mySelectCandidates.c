f6c01c25caca71d66c7a96e931e6b63561ce22c6
gcnnunes <gustavo.cesar.nunes@gmail.com>
2015-09-20 13:14:46 -0300

    alteracoes no mySelectCandidates

--- ift/demo/mySelectCandidates.c
+++ ift/demo/mySelectCandidates.c
@@ -1,4 +1,5 @@
 #include "ift.h"
+#include <stdlib.h>
 
 iftImage* selectCandidates(iftImage *orig) {
 	iftImage *aux;
@@ -15,18 +16,32 @@ iftImage* selectCandidates(iftImage *orig) {
 
 int main(int argc, char *argv[]) {
     iftImage* orig;
-    char inputPath[100], outputPath[100];
+    char inputPath[100], outputPath[100], sedir[100];
     timer *t1 = NULL, *t2 = NULL;
     char outfile[100];
+	FILE *se;
+    char *line;
 
     int MemDinInicial, MemDinFinal;
     MemDinInicial = iftMemoryUsed();
 
-	printf("please input: \t<input images dir>\n\t\t<output images dir>\n");
-	scanf("%s", &inputPath);
-	scanf("%s", &outputPath);
+	printf("please input: \t<input images dir>\n\t\t<output images dir>\n\t\t<SE dir>\n");
+	scanf("%s", inputPath);
+	scanf("%s", outputPath);
+	scanf("%s", sedir);
 	
     iftDir* inputDir = iftLoadFilesFromDirectory(inputPath, "pgm");
+
+	se = fopen(sedir, "r");
+    if (se) {
+		while (fgets(line, sizeof(line), se)) {
+			
+		}
+    }
+    else {
+		printf("SE file not found\n");
+		return (0);
+    }
 
     for (int i = 0; i < inputDir->nfiles; ++i) {
 
@@ -40,7 +55,7 @@ int main(int argc, char *argv[]) {
 
         sprintf(outfile, "%s/%s", outputPath, iftBasename(inputDir->files[i]->pathname));
 
-		printf(outfile);
+		printf("%s", outfile);
         iftWriteImageP2(candidates, outfile);
 
         fprintf(stdout, "%dth image plate candidates located in %f ms\n", i+1,  iftCompTime(t1, t2));
