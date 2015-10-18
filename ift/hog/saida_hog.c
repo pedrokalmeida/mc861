3573e64b92fccb37dc711d3e1678de8930df3d4c
Cibelle Begalli <cib.begalli@gmail.com>
2015-10-18 17:29:34 -0200

    Atualizado saida hog

--- ift/hog/saida_hog.c
+++ ift/hog/saida_hog.c
@@ -4,32 +4,43 @@ int main() {
 	int MemDinInicial, MemDinFinal;
     MemDinInicial = iftMemoryUsed();
 
-	iftImage *img = iftReadImageByExt("orig/0043.pgm");
+	iftDir *orig_dir;	
+    orig_dir = iftLoadFilesFromDirectory("orig", "pgm");
+    char name[100];
 
-	iftImage *img_v1 = firstStep_normalize_v1(img);
-	iftWriteImageP2(img_v1, "result/1_0043_v1_r5_L4095.pgm");
+	for(int i=0; i<orig_dir->nfiles; i++) {
+		iftImage *img = iftReadImageByExt(orig_dir->files[i]->pathname);
+
+		iftImage *img_v1 = firstStep_normalize_v1(img);
+		sprintf(name, "%s/1_v1_r5_L4095_%s", "result", iftBasename(orig_dir->files[i]->pathname));
+		iftWriteImageP2(img_v1, name);
 	
-	iftImage *img_v2 = firstStep_normalize_v2(img);
-	iftWriteImageP2(img_v2, "result/1_0043_v2_r5_K2000.pgm");
+		iftImage *img_v2 = firstStep_normalize_v2(img);
+		sprintf(name, "%s/1_v2_K2000_%s", "result", iftBasename(orig_dir->files[i]->pathname));
+		iftWriteImageP2(img_v2, name);
 
-	iftImage *g_mag;
-	iftImage *g_orient;
-	secondStep_gradient_v2(img_v2, &g_mag, &g_orient);
-	iftWriteImageP2(g_mag, "result/2_0043_v2_r3_mag.pgm");
-	iftWriteImageP2(g_orient, "result/2_0043_v2_r3_orient.pgm");
-	iftDestroyImage(&g_mag);
-	iftDestroyImage(&g_orient);
+		iftImage *g_mag;
+		iftImage *g_orient;
+		secondStep_gradient_v2(img_v2, &g_mag, &g_orient);
+		sprintf(name, "%s/2_v2_r3_mag_%s", "result", iftBasename(orig_dir->files[i]->pathname));
+		iftWriteImageP2(g_mag, name);
+		sprintf(name, "%s/2_v2_r3_orient_%s", "result", iftBasename(orig_dir->files[i]->pathname));
+		iftWriteImageP2(g_orient, name);
+		iftDestroyImage(&g_mag);
+		iftDestroyImage(&g_orient);
 	
-	secondStep_gradient_v2(img_v1, &g_mag, &g_orient);
-	iftWriteImageP2(g_mag, "result/2_0043_v1_r3_mag.pgm");
-	iftWriteImageP2(g_orient, "result/2_0043_v1_r3_orient.pgm");
+		secondStep_gradient_v2(img_v1, &g_mag, &g_orient);
+		sprintf(name, "%s/2_v1_r3_mag_%s", "result", iftBasename(orig_dir->files[i]->pathname));
+		iftWriteImageP2(g_mag, name);
+		sprintf(name, "%s/2_v1_r3_orient_%s", "result", iftBasename(orig_dir->files[i]->pathname));
+		iftWriteImageP2(g_orient, name);
 
-    iftDestroyImage(&img);
-    iftDestroyImage(&img_v1);
-	iftDestroyImage(&img_v2);
-	iftDestroyImage(&g_mag);
-	iftDestroyImage(&g_orient);
-
+		iftDestroyImage(&img);
+		iftDestroyImage(&img_v1);
+		iftDestroyImage(&img_v2);
+		iftDestroyImage(&g_mag);
+		iftDestroyImage(&g_orient);
+	}
 	MemDinFinal = iftMemoryUsed();
     if (MemDinInicial != MemDinFinal)
         printf("\n\nDinamic memory was not completely deallocated (%d, %d)\n",
